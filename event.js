cea56e3351f3d7811bcee4d49c35dd885a9fce46
Emily Chen <tenshiemi@gmail.com>
2014-10-18 15:30:21 +0200

    working on popup feature

--- event.js
+++ event.js
@@ -1,38 +1,43 @@
-// Execute the inject.js in a tab and call a method,
-// passing the result to a callback function.
-function injectedMethod (tab, method, callback) {
-  chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function(){
-    chrome.tabs.sendMessage(tab.id, { method: method }, callback);
-  });
-}
+  // Execute the inject.js in a tab and call a method,
+  // passing the result to a callback function.
+  function injectedMethod (tab, method, callback) {
+    chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function(){
+      chrome.tabs.sendMessage(tab.id, { method: method }, callback);
+    });
+  }
 
-function copyToClipboard( text ){
-                var copyDiv = document.createElement('div');
-                copyDiv.contentEditable = true;
-                document.body.appendChild(copyDiv);
-                copyDiv.innerHTML = text;
-                copyDiv.unselectable = "off";
-                copyDiv.focus();
-                document.execCommand('SelectAll');
-                document.execCommand("Copy", false, null);
-                document.body.removeChild(copyDiv);
-            }
+  function copyToClipboard( text ){
+                  var copyDiv = document.createElement('div');
+                  copyDiv.contentEditable = true;
+                  document.body.appendChild(copyDiv);
+                  copyDiv.innerHTML = text;
+                  copyDiv.unselectable = "off";
+                  copyDiv.focus();
+                  document.execCommand('SelectAll');
+                  document.execCommand("Copy", false, null);
+                  document.body.removeChild(copyDiv);
+              }
 
-function getTeam (tab) {
-  // When we get a result back from the getTeam
-  // method, copy the team info to the clipboard
-  injectedMethod(tab, 'getTeam', function (response) {
-    var team = response.data;
-    var mstring = ""
-    for (var i = 0; i < team.length; i++) {
-    	mstring += ("* " + team[i].name + " (" + team[i].position + ") vs " + team[i].opponent) + "  ";
-    }
-    copyToClipboard(mstring);
-    alert("Copied team to clipboard!");
-    return true;
-  });
-}
+  function getTeam (tab) {
+    // When we get a result back from the getTeam
+    // method, copy the team info to the clipboard
+    injectedMethod(tab, 'getTeam', function (response) {
+      console.log("hi");
+      var team = response.data;
+      var mstring = ""
+      for (var i = 0; i < team.length; i++) {
+      	mstring += ("* " + team[i].name + " (" + team[i].position + ") vs " + team[i].opponent) + "  ";
+      }
+      copyToClipboard(mstring);
+      alert("Copied team to clipboard!");
+      return true;
+    });
+  }
 
-// When the browser action is clicked, call the
-// getTeam function.
-chrome.browserAction.onClicked.addListener(getTeam);
+  // When the browser action is clicked, call the
+  // getTeam function.
+  chrome.browserAction.onClicked.addListener(getTeam);
+
+// document.addEventListener('DOMContentLoaded', function () {
+//       document.getElementById("full_team").addEventListener('click', getTeam);;     
+// });
